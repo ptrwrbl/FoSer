2713218f757cae5fd9d6f012b809f6ac92f86c17
ptrwrbl <pirrowsky@gmail.com>
2021-11-25 18:47:53 +0100

    Implementing foreground service and notification updates (Lab09)

--- app/src/main/java/pollub/pam/foser/MyForegroundService.java
+++ app/src/main/java/pollub/pam/foser/MyForegroundService.java
@@ -5,14 +5,18 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MyForegroundService extends Service {
     public static final String CHANNEL_ID = "MyForegroundServiceChannel";
@@ -25,12 +29,60 @@ public class MyForegroundService extends Service {
 
     private String jobMessage;
     private Boolean jobTime, jobSync, jobDoubleSpeed;
+    private final long jobPeriod = 2000;
+
+    private Context ctx;
+    private Intent notificationIntent;
+    private PendingIntent pendingIntent;
+
+    private int jobCounter;
+    private Timer jobTimer;
+    private TimerTask timerTask;
+    final Handler handler = new Handler();
+
+    final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Notification notification = new Notification.Builder(ctx, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.circle)
+                    .setContentTitle(getString(R.string.service_title))
+                    .setShowWhen(jobTime)
+                    .setContentText(jobMessage + " " + String.valueOf(jobCounter))
+                    .setLargeIcon(BitmapFactory.decodeResource (getResources() , R.drawable.circle ))
+                    .setContentIntent(pendingIntent)
+                    .build();
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.notify(1, notification);
+        }
+    };
 
     @Override
-    public void onCreate() { super.onCreate(); }
+    public void onCreate() {
+        super.onCreate();
+        ctx = this;
+        notificationIntent = new Intent(ctx, MainActivity.class);
+        pendingIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
+        jobCounter = 0;
+        jobTimer = new Timer();
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                jobCounter++;
+                handler.post(runnable);
+            }
+        };
+
+    }
 
     @Override
-    public void onDestroy() { super.onDestroy(); }
+    public void onDestroy() {
+        handler.removeCallbacks(runnable);
+        jobTimer.cancel();
+        jobTimer.purge();
+        jobTimer = null;
+        super.onDestroy();
+    }
 
     @Nullable
     @Override
@@ -38,17 +90,16 @@ public class MyForegroundService extends Service {
         return null;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
         jobMessage = intent.getStringExtra(JOB_MESSAGE);
         jobTime = intent.getBooleanExtra(JOB_TIME,false);
         jobSync = intent.getBooleanExtra(JOB_SYNC,false);
         jobDoubleSpeed = intent.getBooleanExtra(JOB_DOUBLE_SPEED,false);
 
         createNotificationChannel();
-
-        Intent notificationIntent = new Intent(this,MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
 
         Notification notification = new Notification.Builder(this,CHANNEL_ID)
                 .setSmallIcon(R.drawable.circle)
@@ -67,12 +118,9 @@ public class MyForegroundService extends Service {
     }
 
     private void doWork() {
-        String info = "Start working..."    + "\n"
-                + "time: "         + jobTime.toString()    + "\n"
-                + "sync: "         + jobSync.toString()    + "\n"
-                + "double_speed: " + jobDoubleSpeed.toString();
-
-        Toast.makeText(this, info ,Toast.LENGTH_LONG).show();
+        if(jobSync) {
+            jobTimer.schedule(timerTask, 0L, jobDoubleSpeed ? jobPeriod / 2L : jobPeriod);
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
